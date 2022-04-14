ad35de7d91a09eaaa415c120b3b9d4ed380ef721
Fabian Soriano <fabo.soriano@gmail.com>
2022-04-14 00:07:04 -0700

    made changes from assignment

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Select some elements...
+
+/*  Step 1:  Selecting element H2 by defining header
+that is identified by ID #page-header
+OR this is also known as defining value header as a DOM element */
+
+let header = document.querySelector('#page-header')
+
+// Step 2: modify header with style and textALign for left
+header.style.textAlign = "left"
+
+/*Step 3: slect all elements represented by img tags with the 
+appropriate propery, class="dog-image". */ 
+let dogImages = document.querySelectorAll(".dog-image")
+
+/*Step 4: Modify these multiple elements with a for loop 
+by changing the style.borderRadius */
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px',
+    dogImages[i].style.transform = "rotate(180deg)"
+}
+
+//Change dogs' names to align right
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'right'
+}
+
+//Change color of footer text
+let footerText = document.querySelector('.footer')
+footerText.style.color = 'red'
+
+//Flip images upside down. Action completed in line 21.
